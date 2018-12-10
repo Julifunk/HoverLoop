ed885afba5de5138e219f33befcbeb8c6dc80745
Jo10 <Johannes.Wiesneth@stud.uni-regensburg.de>
2018-12-11 00:12:57 +0100

    moving obstacles
    moving obstacles

---
+++ Scene2/Assets/WallController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+
+	public float Speed = 15f;
+
+	public float StartPosition;
+	public float EndPosition;
+
+	private float _direction = 1f;
+
+	private void Update()
+	{
+		if ( transform.position.x < StartPosition || transform.position.x > EndPosition)
+		{
+			_direction *= -1;
+		}
+	}
+
+	// Update is called once per frame
+	private void FixedUpdate()
+	{
+		transform.Translate(Speed * Time.deltaTime * _direction,0 ,0);
+	}
+	
+    
+}
